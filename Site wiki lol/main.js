2a5e3fd1cccf967589b14e5711dc2f6ac5fcecd8
Antoine Rinaldi <antoine.rinaldi@etu.univ-amu.fr>
2025-01-08 22:46:11 +0100

    Add files via upload

---
+++ Site wiki lol/main.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('theme-toggle');
+const icon = document.getElementById('theme-icon');
+  
+    button.addEventListener('click', () => {
+        document.body.classList.toggle('t22');
+  
+        if (document.body.classList.contains('t22')) {
+            icon.src = 'Images et video/t2.png';
+        }
+        else {
+            icon.src = 'Images et video/t1.png';
+        }
+    });
